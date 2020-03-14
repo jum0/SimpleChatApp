bb0fb16225d2df9a948b12b6d0b5d1b7b4994f67
Junmo Han <wnsah052@naver.com>
2020-03-14 22:51:11 +0900

    renamed

--- SImpleChatApp/Pods/Target Support Files/Baraba/Baraba-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Baraba : NSObject
-@end
-@implementation PodsDummy_Baraba
-@end
--- SImpleChatApp/Pods/Target Support Files/Pods-SimpleChatApp/Pods-SimpleChatApp-frameworks.sh
+++ SImpleChatApp/Pods/Target Support Files/Pods-SimpleChatApp/Pods-SimpleChatApp-frameworks.sh
@@ -161,7 +161,6 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Baraba/Baraba.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/BoringSSL-GRPC/openssl_grpc.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CLTypingLabel/CLTypingLabel.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
@@ -173,7 +172,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Baraba/Baraba.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/BoringSSL-GRPC/openssl_grpc.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CLTypingLabel/CLTypingLabel.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
